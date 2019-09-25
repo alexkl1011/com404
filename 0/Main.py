0e04e026b5190df5173b24f4d53fd29ede0abdf1
alexkl1011 <3kella22@solent.ac.uk>
2019-09-25 11:33:45 +0000

    changing eyes

--- 0/Main.py
+++ 0/Main.py
@@ -1,1 +1,15 @@
-print("Hello")
+
+
+name = input("pealse enter a name")
+age = int(input("please enter an age"))
+height = 1.8
+initial = 'A'
+isGreat = True
+
+print("Hello, " + name + " you are " + str(age)) 
+
+print("yay"*10)
+
+
+
+
---
+++ 1-basics/1-output/4-ascii-art/bot.py
@@ -0,0 +1,14 @@
+eyes = input("enter eyes")
+mouth = input("enter mouth")
+
+# Display a box
+print("##########")
+print("#  "+eyes+"   "+eyes+" #")
+print("# "+mouth+" #")
+print("##########")
+print("//  ||||  \\")
+print("    ||||  ")
+print("    ||||  ")
+print("    ||||  ")
+print("    ||||  ")
+print("  --   - -")
